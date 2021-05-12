e0e6f3a9b664405c4c238c9ddf0ec59a86803f58
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-05-11 22:14:22 -0300

    numberOfSections and numberOfRowsInSection

--- App_Signos/App_Signos/ViewController.swift
+++ App_Signos/App_Signos/ViewController.swift
@@ -25,7 +25,14 @@ class ViewController: UITableViewController {
         signoDescription.append("Aquariano")
         signoDescription.append("Peixinho")
     }
-
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
 
 }
 
